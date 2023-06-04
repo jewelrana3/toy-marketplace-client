0f3a64e1fc2e4c0d849ec374e04e89f430f90520
jewelrana3 <juyelrana7752@gmail.com>
2023-06-04 09:00:20 +0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,20 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.process.env.VITE_apiKey,
-  // authDomain: import.meta.process.env.VITE_authDomain,
-  // projectId: import.meta.process.env.VITE_projectId,
-  // storageBucket: import.meta.process.env.VITE_storageBucket,
-  // messagingSenderId: import.meta.process.env.VITE_messagingSenderId,
-  // appId: import.meta.process.env.VITE_appId,
+  apiKey: import.meta.process.env.VITE_apiKey,
+  authDomain: import.meta.process.env.VITE_authDomain,
+  projectId: import.meta.process.env.VITE_projectId,
+  storageBucket: import.meta.process.env.VITE_storageBucket,
+  messagingSenderId: import.meta.process.env.VITE_messagingSenderId,
+  appId: import.meta.process.env.VITE_appId,
 
 
-apiKey : "AIzaSyBdPrGiyxHK3MrpKjkPMik7KVypp8E0YxU",
-authDomain : "assignment-11-client-15991.firebaseapp.com",
-projectId : "assignment-11-client-15991",
-storageBucket : "assignment-11-client-15991.appspot.com",
-messagingSenderId : "516805797809",
-appId : "1:516805797809:web:032a676260d10a95577f9",
+// apiKey : "AIzaSyBdPrGiyxHK3MrpKjkPMik7KVypp8E0YxU",
+// authDomain : "assignment-11-client-15991.firebaseapp.com",
+// projectId : "assignment-11-client-15991",
+// storageBucket : "assignment-11-client-15991.appspot.com",
+// messagingSenderId : "516805797809",
+// appId : "1:516805797809:web:032a676260d10a95577f9",
 };
 
 // Initialize Firebase
