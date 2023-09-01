4d6f8b25cc97b0a881b7248a2d054277b93c7b82
Jewel Rana <juyelrana7752@gmail.com>
2023-09-01 05:59:15 -0700

    dynamic title

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 // import Header from "./Header";
 
@@ -10,26 +11,29 @@ const Contact = () => {
 
     return (
         <>
+            <Helmet>
+                <title>Blogs</title>
+            </Helmet>
             <h2 className="bg-black pt-36 pb-6 text-3xl text-white text-center">Contact </h2>
             <div className="contact-form bg-black pb-24  flex items-center justify-center">
                 <form className="border p-8" onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
 
-                   
-                        <div className="input-header sm:flex gap-8">
-                            <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  First name" /><br />
-                            <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="   Last name" /><br />
-                        </div>
-                        <div className="input-header sm:flex gap-8 mt-4">
-                            <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  Email" /><br />
-                            <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  Phone" /><br />
-                        </div>
 
-                        <div className="mt-4">
-                            <textarea className="rounded py-2 mr-7 resize-none" style={{ }} {...register("aboutYou")} placeholder="  Your Message" />
-                        </div>
-                        <p>{data}</p>
-                        <input className="submit text-white  px-8 py-2 mt-8 rounded-sm border" type="submit" />
-                    
+                    <div className="input-header sm:flex gap-8">
+                        <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  First name" /><br />
+                        <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="   Last name" /><br />
+                    </div>
+                    <div className="input-header sm:flex gap-8 mt-4">
+                        <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  Email" /><br />
+                        <input className="ptag p-2 px-14 rounded" {...register("firstName")} placeholder="  Phone" /><br />
+                    </div>
+
+                    <div className="mt-4">
+                        <textarea className="rounded py-2 mr-7 resize-none" style={{}} {...register("aboutYou")} placeholder="  Your Message" />
+                    </div>
+                    <p>{data}</p>
+                    <input className="submit text-white  px-8 py-2 mt-8 rounded-sm border" type="submit" />
+
                 </form>
             </div>
         </>
--- src/pages/Sales/Sales.jsx
+++ src/pages/Sales/Sales.jsx
@@ -1,69 +1,75 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Sales = () => {
     return (
-        <div className="px-2 pt-40 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
-            <div className="card  glass ">
-                <figure><img src="https://i.ibb.co/5BRfgQs/istockphoto-1069600900-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
+        <>
+            <Helmet>
+                <title>Blogs</title>
+            </Helmet>
+            <div className="px-2 pt-40 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+                <div className="card  glass ">
+                    <figure><img src="https://i.ibb.co/5BRfgQs/istockphoto-1069600900-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card gap-8 glass ">
+                    <figure><img src="https://i.ibb.co/9qRLd3S/istockphoto-1359811855-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  glass ">
+                    <figure><img src="https://i.ibb.co/17Xs4Zg/istockphoto-539350870-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  glass ">
+                    <figure><img src="https://i.ibb.co/M57sVjp/istockphoto-1196393819-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  glass ">
+                    <figure><img src="https://i.ibb.co/GMLB4TF/istockphoto-939438514-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card  glass ">
+                    <figure><img src="https://i.ibb.co/KKmgKnj/istockphoto-1097258406-612x612.jpg" alt="car!" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Swimsuit with Ruffles</h2>
+                        <p>$13.00</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Options</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="card gap-8 glass ">
-                <figure><img src="https://i.ibb.co/9qRLd3S/istockphoto-1359811855-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card  glass ">
-                <figure><img src="https://i.ibb.co/17Xs4Zg/istockphoto-539350870-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card  glass ">
-                <figure><img src="https://i.ibb.co/M57sVjp/istockphoto-1196393819-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card  glass ">
-                <figure><img src="https://i.ibb.co/GMLB4TF/istockphoto-939438514-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card  glass ">
-                <figure><img src="https://i.ibb.co/KKmgKnj/istockphoto-1097258406-612x612.jpg" alt="car!" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Swimsuit with Ruffles</h2>
-                    <p>$13.00</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Options</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </>
     );
 };
 
