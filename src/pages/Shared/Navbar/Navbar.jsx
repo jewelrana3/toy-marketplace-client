71149494c23afd14d11e72d8cd18ad553b75817d
Jewel Rana <juyelrana7752@gmail.com>
2023-09-02 20:11:44 -0700

    overview hidden complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -33,14 +33,14 @@ const Navbar = () => {
         <div className="navbar fixed z-10  bg-slate-700">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost text-white lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost text-white ml-6 lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content z-50 mt-3 p-2 shadow bg-stone-500 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content z-50 mt-3 ml-12 p-2 shadow bg-stone-500 rounded-box w-52">
                         {navItems}
                     </ul>
                 </div>
-                <div className="flex ml-4">
+                <div className="flex">
                     <img style={{ width: '50px' }} src={logo} alt="" />
                     <a className="btn btn-ghost normal-case text-xl text-white">BrainYToyStroe</a>
                 </div>
